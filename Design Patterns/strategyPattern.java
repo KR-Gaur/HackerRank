ea3dec105aa3e7111f19c809dd5a794961d68c47
Kailash Raj Gaur <kailashgaur10@gmail.com>
2021-03-28 17:18:05 +0530

    Create strategyPattern.java

---
+++ Design Patterns/strategyPattern.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+interface Strategy{
+    void move();
+}
+
+class Car implements Strategy{
+    public void move(){
+        System.out.println("Going via Car");
+    } 
+}
+
+class Bus implements Strategy{
+    public void move(){
+        System.out.println("Going via Bus");
+    } 
+}
+
+class Cycle implements Strategy{
+    public void move(){
+        System.out.println("Going via Cycle");
+    } 
+}
+
+class Travel{
+    
+    Strategy stgy;
+
+    Travel(Strategy s){
+        stgy = s;
+    }
+
+    public void setStgy(Strategy stgy) {
+        this.stgy = stgy;
+    }
+
+    public void mode(){
+        System.out.print("Mode : ");
+        stgy.move();
+    }
+
+}
+
+public class strategyPattern {
+    public static void main(String[] args) {
+    
+        Cycle cycle = new Cycle();
+        Bus bus = new Bus();
+        Car car = new Car();
+
+        System.out.println("Initially ");
+        Travel t = new Travel(cycle);
+        t.mode();
+        
+        System.out.println("\nThen Changed to ");
+        t.setStgy(car);
+        t.mode();
+        
+        System.out.println("\nAgain Changed to ");
+        t.setStgy(bus);
+        t.mode();
+    }    
+}
